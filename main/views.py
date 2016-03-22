66ace3d8797982cd2313a726a456e2a520da3720
gqdw <aca_jingru@qq.com>
2016-03-22 22:09:27 +0800

    change template and views

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Key,Host
 # Create your views here.
 def index(req):
-	return render(req, 'index.html', {})
+	hosts = Host.objects.all()
+	print Host.objects.count()
+	print Host.objects.all()
+	print '123'
+	return render(req, 'index.html', {'hosts':hosts})
 	
