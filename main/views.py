89b282951758bdd48403c568b9567bdf06c786c5
gqdw <aca_jingru@qq.com>
2016-03-23 14:37:58 +0800

    remove commit

--- main/views.py
+++ main/views.py
@@ -4,9 +4,7 @@ from .models import Key,Host
 # Create your views here.
 def index(req):
 	hosts = Host.objects.all()
-	print Host.objects.count()
-	print Host.objects.all()
-	print '123'
+	# print Host.objects.count()
 	return render(req, 'index.html', {'hosts':hosts})
 	
 def read(req):
