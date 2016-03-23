061115b36302ddb3694fea997dca9947b3d12575
gqdw <aca_jingru@qq.com>
2016-03-23 13:58:33 +0800

    add HostManager

--- main/models.py
+++ main/models.py
@@ -8,12 +8,20 @@ class Key(models.Model):
 
 	def __unicode__(self):
 		return self.name
+
+
+class HostManager(models.Manager):
+	def create_host(self, hostname, eth0, eth1):
+		host = self.create(hostname=hostname, eth0_ip=eth0, eth1_ip=eth1)
+		return host
+
+
 class Host(models.Model):
 	hostname = models.CharField(max_length=30)
 	eth0_ip = models.GenericIPAddressField()
 	eth1_ip = models.GenericIPAddressField()
+	objects = HostManager()
 
 	def __unicode__(self):
 		return self.hostname
-
 
